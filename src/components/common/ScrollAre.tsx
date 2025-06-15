3b95b5b5214756de963c45dc52d4926d4f686567
kyyril <khairilrahman55@gmail.com>
2025-06-15 07:18:51 +0700

    refactor: Remove common components and icons

--- src/components/common/ScrollAre.tsx
+++
@@ -1,8 +0,0 @@
-interface ScrollAreaProps {
-  children: React.ReactNode;
-  className?: string;
-}
-export const ScrollArea: React.FC<ScrollAreaProps> = ({
-  children,
-  className = "",
-}) => <div className={`overflow-auto ${className}`}>{children}</div>;
